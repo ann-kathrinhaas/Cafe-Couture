88430529ffe852c7a0e4659f120056a658ad66e3
ann-kathrinhaas <102215660+ann-kathrinhaas@users.noreply.github.com>
2025-05-18 20:05:57 +0200

    added instagram slider

---
+++ js/slider.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 2,
+  centeredSlides: true,
+  initialSlide: 0,
+//   loop: true,
+//   loopedSlides: 3,
+  spaceBetween: 0,
+  autoplay: {
+    delay: 3000,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
