cbe16662b736ed7adb608c18d6ca2f6bdef8ffa4
ann-kathrinhaas <102215660+ann-kathrinhaas@users.noreply.github.com>
2025-06-21 13:58:44 +0200

    fixed animations on mobile

--- js/main.js
+++ js/main.js
@@ -1,18 +1,14 @@
 const observer = new IntersectionObserver((entries, observer) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      const animatables = entry.target.querySelectorAll('.fact, .product, .slide-in-left, .slide-in-right');
-      animatables.forEach(el => el.classList.add('animate'));
-
+      entry.target.classList.add('animate');
       observer.unobserve(entry.target);
     }
   });
 }, {
-  threshold: 0.6
+  threshold: window.innerWidth <= 768 ? 0.3 : 0.6
 });
 
-document.querySelectorAll('.module-wrapper').forEach(section => {
-  if (section.querySelector('.fact, .product, .slide-in-left, .slide-in-right')) {
-    observer.observe(section);
-  }
+document.querySelectorAll('.fact, .product, .slide-in-left, .slide-in-right').forEach(el => {
+  observer.observe(el);
 });
